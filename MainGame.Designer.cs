eb829f60ba9f7bd5a0af210d479ad6b6e183d942
BrianMac <87391503+BrianMac@users.noreply.github.com>
2022-01-27 12:31:28 -0500

    Main Game logic/functionality finished
    Main Game logic/functionality finished.

--- MainGame.Designer.cs
+++ MainGame.Designer.cs
@@ -35,12 +35,17 @@
             this.lblDifficulty = new System.Windows.Forms.Label();
             this.lblQuote = new System.Windows.Forms.Label();
             this.lblAuthor = new System.Windows.Forms.Label();
+            this.rtbQuote = new System.Windows.Forms.RichTextBox();
+            this.cbxLetters = new System.Windows.Forms.ComboBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.clbGuesses = new System.Windows.Forms.CheckedListBox();
+            this.lblHealth = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblDifficulty
             // 
             this.lblDifficulty.AutoSize = true;
-            this.lblDifficulty.Location = new System.Drawing.Point(209, 110);
+            this.lblDifficulty.Location = new System.Drawing.Point(17, 21);
             this.lblDifficulty.Name = "lblDifficulty";
             this.lblDifficulty.Size = new System.Drawing.Size(106, 25);
             this.lblDifficulty.TabIndex = 0;
@@ -49,7 +54,7 @@
             // lblQuote
             // 
             this.lblQuote.AutoSize = true;
-            this.lblQuote.Location = new System.Drawing.Point(115, 602);
+            this.lblQuote.Location = new System.Drawing.Point(17, 490);
             this.lblQuote.Name = "lblQuote";
             this.lblQuote.Size = new System.Drawing.Size(82, 25);
             this.lblQuote.TabIndex = 1;
@@ -58,17 +63,125 @@
             // lblAuthor
             // 
             this.lblAuthor.AutoSize = true;
-            this.lblAuthor.Location = new System.Drawing.Point(115, 669);
+            this.lblAuthor.Location = new System.Drawing.Point(12, 793);
             this.lblAuthor.Name = "lblAuthor";
             this.lblAuthor.Size = new System.Drawing.Size(87, 25);
             this.lblAuthor.TabIndex = 2;
             this.lblAuthor.Text = "Author: ";
             // 
+            // rtbQuote
+            // 
+            this.rtbQuote.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.rtbQuote.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbQuote.Font = new System.Drawing.Font("OCR A Extended", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbQuote.Location = new System.Drawing.Point(17, 518);
+            this.rtbQuote.Name = "rtbQuote";
+            this.rtbQuote.Size = new System.Drawing.Size(1044, 263);
+            this.rtbQuote.TabIndex = 3;
+            this.rtbQuote.Text = "";
+            // 
+            // cbxLetters
+            // 
+            this.cbxLetters.FormattingEnabled = true;
+            this.cbxLetters.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z"});
+            this.cbxLetters.Location = new System.Drawing.Point(940, 75);
+            this.cbxLetters.Name = "cbxLetters";
+            this.cbxLetters.Size = new System.Drawing.Size(121, 33);
+            this.cbxLetters.TabIndex = 4;
+            this.cbxLetters.SelectedIndexChanged += new System.EventHandler(this.cbxLetters_SelectedIndexChanged);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Enabled = false;
+            this.btnGuess.Location = new System.Drawing.Point(940, 21);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(121, 48);
+            this.btnGuess.TabIndex = 5;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // clbGuesses
+            // 
+            this.clbGuesses.Enabled = false;
+            this.clbGuesses.FormattingEnabled = true;
+            this.clbGuesses.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z"});
+            this.clbGuesses.Location = new System.Drawing.Point(1087, 21);
+            this.clbGuesses.Name = "clbGuesses";
+            this.clbGuesses.Size = new System.Drawing.Size(66, 760);
+            this.clbGuesses.TabIndex = 6;
+            // 
+            // lblHealth
+            // 
+            this.lblHealth.AutoSize = true;
+            this.lblHealth.Location = new System.Drawing.Point(17, 59);
+            this.lblHealth.Name = "lblHealth";
+            this.lblHealth.Size = new System.Drawing.Size(86, 25);
+            this.lblHealth.TabIndex = 7;
+            this.lblHealth.Text = "Health: ";
+            // 
             // MainGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1174, 1129);
+            this.ClientSize = new System.Drawing.Size(1174, 832);
+            this.Controls.Add(this.lblHealth);
+            this.Controls.Add(this.clbGuesses);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.cbxLetters);
+            this.Controls.Add(this.rtbQuote);
             this.Controls.Add(this.lblAuthor);
             this.Controls.Add(this.lblQuote);
             this.Controls.Add(this.lblDifficulty);
@@ -87,5 +200,10 @@
         private System.Windows.Forms.Label lblDifficulty;
         private System.Windows.Forms.Label lblQuote;
         private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.RichTextBox rtbQuote;
+        private System.Windows.Forms.ComboBox cbxLetters;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.CheckedListBox clbGuesses;
+        private System.Windows.Forms.Label lblHealth;
     }
 }
--- MainGame.cs
+++ MainGame.cs
@@ -16,6 +16,9 @@ namespace Hangman
         //http://quotes.stormconsultancy.co.uk/api   A hand picked selection of quotes from the field of computer science.
 
         private int difficultyLvl = -1;
+        private int health = 0;
+        private string quoteString = "";
+        private string hiddenQuote = "";
         public MainGame()
         {
             InitializeComponent();
@@ -25,8 +28,41 @@ namespace Hangman
         private async Task GetNewPhrase()
         {
             var quote = await GetPhrase.LoadPhrase();
-            lblQuote.Text += quote.quote;
+            string trueQuote = quote.quote;
+            string hiddenQuote = HideQuote(quote.quote);
+            rtbQuote.Text = hiddenQuote;
             lblAuthor.Text += quote.author;
+        }
+
+        private string HideQuote(string quote)
+        {
+            StringBuilder full = new StringBuilder();
+            StringBuilder hidden = new StringBuilder();
+
+            foreach (char c in quote)
+            {
+                if (c.ToString() == " " || c.ToString().ToUpper() != c.ToString().ToLower())
+                {
+                    full.Append(c);
+                }
+            }
+
+            quoteString = full.ToString();
+
+            foreach (char c in quoteString)
+            {
+                if (c.ToString() != " ")
+                {
+                    hidden.Append('-');
+                }
+                else
+                {
+                    hidden.Append(c);
+                }
+            }
+            hiddenQuote = hidden.ToString();
+            lblHealth.Text = "Health: " + health.ToString();
+            return hiddenQuote;
         }
 
         private void MainGame_Load(object sender, EventArgs e)
@@ -37,18 +73,22 @@ namespace Hangman
             {
                 case 0:
                     lblDifficulty.Text += "Easy";
+                    health = 9;
                     break;
 
                 case 1:
                     lblDifficulty.Text += "Moderate";
+                    health = 6;
                     break;
 
                 case 2:
                     lblDifficulty.Text += "Hard";
+                    health = 3;
                     break;
 
                 case 3:
                     lblDifficulty.Text += "Extreme";
+                    health = 1;
                     break;
 
             }
@@ -56,7 +96,6 @@ namespace Hangman
             GetNewPhrase();
 
         }
-
 
         private void MainGame_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -70,6 +109,73 @@ namespace Hangman
                 else
                 { e.Cancel = true; }
             }
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            if (cbxLetters.SelectedIndex != -1)
+            {
+                if (quoteString.ToUpper().Contains(cbxLetters.SelectedItem.ToString().ToUpper()))
+                {
+                    RevealLetter(Convert.ToChar(cbxLetters.SelectedItem));
+                }
+                else
+                {
+                    health = health - 1;
+                    if (health <= 0)
+                    {
+                        lblHealth.Text = "Health: DEAD";
+                        MessageBox.Show("You Lose!!!");
+                    }
+                    else
+                    {
+                        lblHealth.Text = "Health: " + health.ToString();
+                    }
+                }
+
+                UpdateGuessList(cbxLetters.SelectedIndex);
+                cbxLetters.SelectedIndex = -1;
+            }
+        }
+
+        private void UpdateGuessList(int selected)
+        {
+            clbGuesses.SetItemChecked(selected, true);
+        }
+
+        private void RevealLetter(char letter)
+        {
+            char[] arr = hiddenQuote.ToCharArray();
+            string newHidden;
+
+            for (int i = 0; i < hiddenQuote.Length; i++)
+            {
+                if (quoteString.ToCharArray()[i].ToString().ToUpper() == letter.ToString().ToUpper())
+                {
+                    arr[i] = quoteString.ToCharArray()[i];
+                }
+            }
+            newHidden = new string(arr);
+            hiddenQuote = newHidden;
+
+            rtbQuote.Text = hiddenQuote;
+            if (hiddenQuote == quoteString)
+            {
+                MessageBox.Show("You Win!!!");
+            }
+        }
+
+        private void cbxLetters_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbxLetters.SelectedIndex != -1 && clbGuesses.GetItemCheckState(cbxLetters.SelectedIndex) != CheckState.Checked)
+            {
+                btnGuess.Enabled = true;
+            }
+            else
+            {
+                btnGuess.Enabled = false;
+                cbxLetters.SelectedIndex = -1;
+            }                  
         }
 
     }
